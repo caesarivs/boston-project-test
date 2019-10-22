fdb59aa9d8f80030b718125760c38f094b98a3e1
Caesarivs <caesarivs@gmail.com>
2019-10-21 23:02:59 -0500

    Register view

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,59 +1,58 @@
 <template>
   <div class="register">
-    <form>
+    <form class="form-box">
       <h2>Register</h2>
       <div class="form-group">
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="AM" id="admin" value="Admin" v-model="account_type" required>
-          <label class="form-check-label" for="inlineRadio1">Admin</label>
+          <input class="form-check-input" type="radio" id="admin" value="Admin" v-model="account_type">
+          <label class="form-check-label" for="admin">Admin</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="AM" id="manager" value="Manager" v-model="account_type" required>
-          <label class="form-check-label" for="inlineRadio2">Manager</label>
+          <input class="form-check-input" type="radio" id="manager" value="Manager" v-model="account_type">
+          <label class="form-check-label" for="manager">Manager</label>
         </div>
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" id="name" placeholder="Enter name" v-model="name" required>
+        <input type="text" class="form-control" id="name" placeholder="Enter your name" v-model="name" required>
       </div>
       <div class="form-group">
-        <input type="email" class="form-control" id="email" placeholder="Enter email" v-model="email" required>
+        <input type="email" class="form-control" id="email" placeholder="Enter your email" v-model="email" required>
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" id="password" placeholder="Enter password" v-model="password" required>
+        <input type="password" class="form-control" id="password" placeholder="Enter your password" v-model="password" required>
       </div>
       <div class="form-group">
-        <button type="submit" class="btn btn-primary btn-block" @click="submit">Register</button>
+        <button class="btn btn-primary btn-block" @click="register">Register</button>
+      </div>
+      <div class="clearfix">
+        Have an account? <router-link to="/login">Log in</router-link>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-export default {
+import AuthenticationService from '@/services/AuthenticationService'
 
+export default {
+  data () {
+    return {
+      account_type: 'Manager',
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        account_type: this.account_type,
+        name: this.name,
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
 }
 </script>
-
-<style>
-  .register {
-    width: 340px;
-    margin: 50px auto;
-  }
-  .register form {
-    margin-bottom: 15px;
-    background: #f7f7f7;
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-    padding: 30px;
-  }
-  .register h2 {
-    margin: 0px 0px 15px;
-  }
-  .form-control, .btn {
-    min-height: 38px;
-    border-radius: 2px;
-  }
-  .btn {
-    font-size: 15px;
-    font-weight: bold;
-  }
-</style>
