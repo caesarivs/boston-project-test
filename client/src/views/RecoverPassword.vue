ab295dc0d9e7e0135ac69ed0ac122f662b1d495b
Caesarivs <caesarivs@gmail.com>
2019-10-21 23:32:39 -0500

    Password recovery views

---
+++ client/src/views/RecoverPassword.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="reset-password">
+    <form class="form-box">
+      <h2>Recover password</h2>
+      <div class="form-group">
+        <input type="email" class="form-control" id="email" placeholder="Enter your email" v-model="email" required>
+      </div>
+      <div>
+        <button class="btn btn-primary btn-block" @click="recover">Recover</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    recover () {}
+  }
+}
+</script>
---
+++ client/src/views/ResetPassword.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="reset-password">
+    <form class="form-box">
+      <h2>Reset password</h2>
+      <div class="form-group">
+        <input type="text" class="form-control" id="code" placeholder="Enter your code" v-model="code" required>
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" id="password" placeholder="Enter your new password" v-model="password" required>
+      </div>
+      <div>
+        <button class="btn btn-primary btn-block" @click="reset">Reset</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      code: '',
+      password: ''
+    }
+  },
+  methods: {
+    reset () {}
+  }
+}
+</script>
