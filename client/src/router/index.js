a8dadd93ab4fede424f19a1ead8fb40acf11d539
Caesarivs <caesarivs@gmail.com>
2019-10-21 23:04:42 -0500

    Vue routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
 import Register from '@/views/Register.vue'
+import Confirm from '@/views/Confirm.vue'
 import Login from '@/views/Login.vue'
-import ForgotPassword from '@/views/ForgotPassword.vue'
-import Drivers from '@/views/Drivers.vue'
+import RecoverPassword from '@/views/RecoverPassword.vue'
+import ResetPassword from '@/views/ResetPassword.vue'
 import Cars from '@/views/Cars.vue'
+import Drivers from '@/views/Drivers.vue'
 
 Vue.use(VueRouter)
 
@@ -21,24 +23,34 @@ const routes = [
     component: Register
   },
   {
+    path: '/confirm',
+    name: 'confirm',
+    component: Confirm
+  },
+  {
     path: '/login',
     name: 'login',
     component: Login
   },
   {
-    path: '/forgot-password',
-    name: 'forgot-password',
-    component: ForgotPassword
+    path: '/recover-password',
+    name: 'recover-password',
+    component: RecoverPassword
   },
   {
-    path: '/drivers',
-    name: 'drivers',
-    component: Drivers
+    path: '/reset-password',
+    name: 'reset-password',
+    component: ResetPassword
   },
   {
     path: '/cars',
     name: 'cars',
     component: Cars
+  },
+  {
+    path: '/drivers',
+    name: 'drivers',
+    component: Drivers
   }
 ]
 
