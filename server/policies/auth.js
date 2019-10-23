436dffeef2b0e9a93fc3c1ddf42ae64385d9944a
Caesarivs <caesarivs@gmail.com>
2019-10-23 00:05:53 -0500

    authentication backend validations

--- server/policies/auth.js
+++ server/policies/auth.js
@@ -1,7 +1,48 @@
 const Joi = require('@hapi/joi')
 
-module.exports = {
-  register (req, res, next) {
-    next()
+const register = async (req, res, next) => {
+  const schema = Joi.object({
+    account_type: Joi.string().valid("Admin", "Manager").required(),
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  })
+  
+  try {
+    // const value = 
+    await schema.validateAsync(req.body)
+  } catch (error) {
+    switch (error.details[0].context.key) {
+      case 'account_type':
+      res.status(400).send({
+        error: 'Account type can only be either "Admin" or "Manager"'
+      })
+      break
+      case 'name':
+      res.status(400).send({
+        error: 'Name not valid'
+      })
+      break
+      case 'email':
+      res.status(400).send({
+        error: 'You must provide a valid email address'
+      })
+      break
+      case 'password':
+      res.status(400).send({
+        error: 'Password not valid'
+      })
+      break
+      default:
+      res.status(400).send({
+        error: 'Invalid register information'
+      })
+    }
   }
+  
+  next()
+}
+
+module.exports = {
+  register
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth')
-// const authControllerPolicy = require('../policies/auth')
+const authControllerPolicy = require('../policies/auth')
 
 router.get('/', (req, res) => res.send('Nothing to see here )'))
 
-router.post('/register', authController.register)
+router.post('/register',
+  authControllerPolicy.register,
+  authController.register)
 router.post('/confirm', authController.confirm)
 router.post('/resend', authController.resend)
 router.post('/login', authController.login)
