a03cd13917b5f510b1fc45236eb15843a67fcedc
Caesarivs <caesarivs@gmail.com>
2019-10-23 02:21:14 -0500

    NavBar as component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,21 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <router-link class="navbar-brand" to="/">A Transportation Company</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="navbar-collapse collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/cars">Cars</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/drivers">Drivers</router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/login">Login</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/register">Register</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <div class="container">
+    <NavBar />
+    <main class="container">
       <router-view />
-    </div>
+    </main>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style lang="scss">
 @import './styles/custom.scss';
@@ -40,7 +27,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
-.container {
+main.container {
   margin: 25px auto;
 }
 .form-box {
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <router-link class="navbar-brand" to="/">A Transportation Company</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/cars">Cars</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/drivers">Drivers</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login">Login</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Sign Up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
